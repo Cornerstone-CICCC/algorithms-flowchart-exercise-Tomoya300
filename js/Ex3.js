73896a001efbe5333877176241894e3c66011dde
Tomoya <flyingfox193807@gmail.com>
2024-09-17 12:04:56 -0700

    finished Ex3.js

---
+++ js/Ex3.js
@@ -0,0 +1,10 @@
+let num1 = 4
+let num2 = 8
+let num3 = 12
+if (num1 > num2 && num1 > num3) {
+    console.log(num1);
+} else if (num2 > num1 && num2 > num3) {
+    console.log(num2);
+} else {
+    console.log(num3);
+}
